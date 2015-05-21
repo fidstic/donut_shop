0d6482417e2205436fec219f83c077301f82e79a
Matt Herron <Mdherron@gmail.com>
2015-05-20 21:38:54 -0700

    Updated javascript to handle table add

--- donut_shop.js
+++ donut_shop.js
@@ -46,30 +46,40 @@ var Shop = function (name, average, min, max) {
     row.appendChild(cell);
     table.appendChild(row);
 
+    }
+
+    var locations = [];
+    //locations.push(dTown, cHill, sLakeUnion, wWood, ballard);
+    locations.push(new Shop("Downtown", 4.5, 8, 43));
+    locations.push(new Shop("Capitol Hill", 2, 4, 37));
+    locations.push(new Shop("South Lake Union", 6.33, 9, 23));
+    locations.push(new Shop("Wedgewood", 1.25, 2, 28));
+    locations.push(new Shop("Ballard", 3.75, 8, 58));
+
+    for (var k = 0; k < locations.length; k++) {
+      locations[k].fillHead();
   }
 
 
-  //var addStore = function () {
-    //}
+  var NewLocation = function () {
+    var newShop = document.getElementById("location").value;
+    console.log(newShop);
+    console.log(document.getElementById("location").value);
+    var maxHour = document.getElementById("maxCust").value;
+    console.log(maxHour.type);
+    console.log(maxHour);
+    console.log(maxHour + 1);
+    console.log(maxHour = parseInt(maxHour));
+    console.log(maxHour + 1);
+    var minHour = document.getElementById("minCust").value;
+    var ave = document.getElementById("aveCust").value;
 
-  var dTown = new Shop("Downtown", 4.5, 8, 43);
-  var cHill = new Shop("Capitol Hill", 2, 4, 37);
-  var sLakeUnion = new Shop ("South Lake Union", 6.33, 9, 23);
-  var wWood = new Shop ("Wedgewood", 1.25, 2, 28);
-  var ballard = new Shop ("Ballard", 3.75, 8, 58);
+    var newDoShop = new Shop(newShop, maxHour, minHour, ave)
+    locations.push(newDoShop)
+    newDoShop.fillHead();
+}
 
-dTown.fillHead();
-cHill.fillHead();
-sLakeUnion.fillHead();
-wWood.fillHead();
-ballard.fillHead();
-
-console.log(dTown.donuts());
-console.log(cHill.total());
-console.log(sLakeUnion.total());
-console.log(wWood.total());
-console.log(ballard.total());
-
+  document.getElementById("inputButton").addEventListener('click', NewLocation, false);
 
 
 
